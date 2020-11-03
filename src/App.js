c22be8a3fa9c7d87593cea4a96eced188987a2da
MJ <michaljalowik@gmail.com>
2020-11-03 15:07:34 +0100

    lesson 49 finished

--- src/App.js
+++ src/App.js
@@ -1,22 +1,73 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Person from "./Person/Person"
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-          <p>jjj</p>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+//ES6 way
+const app = (props) => {
+  const [personsState, setPersonsState] = useState({
+    persons: [
+      {name: 'max', age: 28},
+      {name: 'manu', age: 3},
+      {name: 'micg', age: 54}
+    ]
+  });
+
+  const [otherState, setOtherState] = useState('some thing');
+
+  console.log(personsState, otherState);
+
+  const swithNameHandler = (newName) => {
+    setPersonsState({persons: [
+      {name: newName, age: 28},
+      {name: 'manu', age: 3},
+      {name: 'micg', age: 34}
+    ]
+  })
+  
   }
-}
 
-export default App;
+  const style = {
+    backgroundColor: 'white',
+    font: 'inherit',
+    border: '1px solid blue',
+    padding: '8px',
+    cursor: 'pionter'
+  }
+
+  const nameChangerHandler = (event) => {
+    setPersonsState({persons: [
+      {name: 'michal', age: 28},
+      {name: event.target.value, age: 3},
+      {name: 'micg', age: 34}
+    ]
+  })
+  }
+
+
+      return (
+        <div className="App">
+         <h1>hi!!</h1>
+         <p>it works!</p>
+         <button style={style} onClick={() => swithNameHandler()}>switch name</button>
+         <Person 
+         name={personsState.persons[0].name} 
+         age={personsState.persons[0].age}
+         click={swithNameHandler.bind(this, 'BIND !')}> MY HOBBIES: RACING1</Person>
+         <Person 
+         name={personsState.persons[1].name} 
+         age={personsState.persons[1].age}
+         changed2={nameChangerHandler}></Person>
+         <Person 
+         name={personsState.persons[2].name} 
+         age={personsState.persons[2].age}> 
+         MY HOBBIES: RACING</Person>
+        </div>
+      );
+      //return React.createElement('div', {className:"App"}, React.createElement('h1',null,'Hi 2 !!!'))
+    }
+  
+export default app;
+
+
+
+
