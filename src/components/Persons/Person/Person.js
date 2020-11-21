5960b5411041d7d422cb8cd59df1a46aa9f3a38a
MJ <michaljalowik@gmail.com>
2020-11-21 12:04:45 +0100

    lesson 110 refs

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -22,6 +22,21 @@ import classes from './Person.css';
 // `;
 
 class Person extends Component {
+    constructor(props){
+        super();
+        this.inputElementRef = React.createRef();
+    }
+
+
+
+
+    componentDidMount(){
+        //this.inputElement.focus();
+        this.inputElementRef.current.focus();
+    }
+
+
+
     render() {
         console.log('[Person.js] rendering...');
         return (
@@ -32,6 +47,8 @@ class Person extends Component {
             <p key="i2">{this.props.children}</p>
             <input
               key="i3"
+            //   ref={(inputEl) => {this.inputElement = inputEl}}
+            ref={this.inputElementRef}
               type="text"
               onChange={this.props.changed}
               value={this.props.name}
