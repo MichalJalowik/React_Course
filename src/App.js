12e190a7c7dfa84fb8f27308c0037d14fb954279
Michał Jałowik <57842170+MichalJalowik@users.noreply.github.com>
2021-03-09 11:14:00 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,96 +1,136 @@
-import React, { useState } from 'react';
-import './App.css';
-import Person from "./Person/Person"
+import React, { useState } from "react";
+import "./App.css";
+import ImageMapper from "react-image-mapper";
 
 //ES6 way
 const app = (props) => {
+  const [moveMsg, setMoveMsg] = useState(null);
 
-  const [personsState, setPersonsState] = useState({
-    persons: [
-      {name: 'max', age: 28},
-      {name: 'manu', age: 3},
-      {name: 'micg', age: 54}
-    ],
-    otherState: 'some other value',
-
-  });
-
-  const [otherState, setOtherState] = useState('some thing');
-
-  const [getShowPersons, setShowPersons] = useState({
-    showPersons: false
-  });
-
-  console.log(personsState, otherState);
-
-  const swithNameHandler = (newName) => {
-    setPersonsState({persons: [
-      {name: newName, age: 28},
-      {name: 'manu', age: 3},
-      {name: 'micg', age: 34}
+  let MAP = {
+    name: "my-map",
+    areas: [
+      {
+        name: "1",
+        shape: "poly",
+        coords: [25, 33, 27, 300, 128, 240, 128, 94],
+        preFillColor: "green",
+        fillColor: "#0000ff"
+      },
+      {
+        name: "2",
+        shape: "poly",
+        coords: [219, 118, 220, 210, 283, 210, 284, 119],
+        preFillColor: "pink",
+        lineWidth: 10,
+        strokeColor: "#0000ff"
+      },
+      {
+        name: "3",
+        shape: "poly",
+        coords: [381, 241, 383, 94, 462, 53, 457, 282],
+        preFillColor: "yellow", // this is mandatory for stroke color to work
+        lineWidth: 10,
+        strokeColor: "#6afd09"
+      },
+      {
+        name: "4",
+        shape: "poly",
+        coords: [245, 285, 290, 285, 274, 239, 249, 238],
+        preFillColor: "red"
+      },
+      {
+        name: "5",
+        shape: "circle",
+        coords: [170, 100, 25],
+        preFillColor: "rgb(255,255,255,0.3)",
+        lineWidth: 2
+      },
+      {
+        name: "6",
+        shape: "rect",
+        coords: [270, 100, 200, 50],
+        lineWidth: 2,
+        preFillColor: "rgba(255, 255, 255, 0.3)",
+        strokeColor: "#6afd09"
+      }
     ]
-  })
-  
-  }
+  };
 
-  const style = {
-    backgroundColor: 'white',
-    font: 'inherit',
-    border: '1px solid blue',
-    padding: '8px',
-    cursor: 'pionter'
-  }
+  var URL = "https://c1.staticflickr.com/5/4052/4503898393_303cfbc9fd_b.jpg";
 
-  const nameChangerHandler = (event) => {
-    setPersonsState({persons: [
-      {name: 'michal', age: 28},
-      {name: event.target.value, age: 3},
-      {name: 'micg', age: 34}
-    ]
-  })
-  }
+  const clicked = (area) => {
+    this.setState({
+      msg: `You clicked on ${area.shape} at coords ${JSON.stringify(
+        area.coords
+      )} !`
+    });
+  };
 
-  const trogglePersonsHandler = () => {
-      const doesShow = getShowPersons.showPersons;
-      setShowPersons({showPersons: !doesShow});
-  }
+  const clickedOutside = (evt) => {
+    const coords = { x: evt.nativeEvent.layerX, y: evt.nativeEvent.layerY };
+    this.setState({
+      msg: `You clicked on the image at coords ${JSON.stringify(coords)} !`
+    });
+  };
 
+  const moveOnImage = (evt) => {
+    const coords = { x: evt.nativeEvent.layerX, y: evt.nativeEvent.layerY };
+    this.setState({
+      moveMsg: `You moved on the image at coords ${JSON.stringify(coords)} !`
+    });
+  };
 
-  let persons = null;
+  const enterArea = (area) => {
+    this.setState({
+      hoveredArea: area,
+      msg: `You entered ${area.shape} ${area.name} at coords ${JSON.stringify(
+        area.coords
+      )} !`
+    });
+  };
 
-  if (getShowPersons.showPersons) {
-    persons = (
-      <div >
-      <Person 
-      name={personsState.persons[0].name} 
-      age={personsState.persons[0].age}
-      click={swithNameHandler.bind(this, 'BIND !')}> MY HOBBIES: RACING1</Person>
-      <Person 
-      name={personsState.persons[1].name} 
-      age={personsState.persons[1].age}
-      changed2={nameChangerHandler}></Person>
-      <Person 
-      name={personsState.persons[2].name} 
-      age={personsState.persons[2].age}> 
-      MY HOBBIES: RACING</Person>
-      </div> 
+  const leaveArea = (area) => {
+    this.setState({
+      hoveredArea: null,
+      msg: `You leaved ${area.shape} ${area.name} at coords ${JSON.stringify(
+        area.coords
+      )} !`
+    });
+  };
+
+  const moveOnArea = (area, evt) => {
+    const coords = { x: evt.nativeEvent.layerX, y: evt.nativeEvent.layerY };
+    setMoveMsg(
+      "You moved on " +
+        area.shape +
+        " " +
+        area.name +
+        " at cords: " +
+        coords.x +
+        " " +
+        coords.y
     );
-  }
+  };
 
+  const getTipPosition = (area) => {
+    return { top: `${area.center[1]}px`, left: `${area.center[0]}px` };
+  };
 
-      return (
-        <div className="App">
-         <h1>hi!!</h1>
-         <p>it works!</p>
-         <button style={style} onClick={trogglePersonsHandler}>Show persons</button>
-        {persons}
-        </div>
-      );
-      //return React.createElement('div', {className:"App"}, React.createElement('h1',null,'Hi 2 !!!'))
-    }
-  
+  return (
+    <div className="App">
+      <h1>hi!!</h1>
+      <p>it works!</p>
+      <ImageMapper
+        src={URL}
+        map={MAP}
+        width={500}
+        onMouseMove={(area, _, evt) => moveOnArea(area, evt)}
+      />
+
+      <div>{moveMsg}</div>
+    </div>
+  );
+  //return React.createElement('div', {className:"App"}, React.createElement('h1',null,'Hi 2 !!!'))
+};
+
 export default app;
-
-
-
-
