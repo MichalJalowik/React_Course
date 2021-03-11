f16f17f273af7d72814482c7da6163e1fa7c0587
Michał Jałowik <57842170+MichalJalowik@users.noreply.github.com>
2021-03-11 07:54:38 +0100

    upd1

--- src/App.js
+++ src/App.js
@@ -7,39 +7,7 @@ const app = (props) => {
   const [hoveredArea, setHoveredArea] = useState(null);
   const [moveMsg, setMoveMsg] = useState(null);
 
-  const [MAP2, setMAP2] = useState({
-    name: "my-map",
-    areas: [
-      {
-        name: "1",
-        shape: "poly",
-        coords: [25, 33, 27, 300, 128, 240],
-        preFillColor: "green",
-        fillColor: "blue"
-      },
-      {
-        name: "2",
-        shape: "poly",
-        coords: [219, 118, 220, 210, 283, 210, 284, 119],
-        preFillColor: "pink"
-      },
-      {
-        name: "3",
-        shape: "poly",
-        coords: [381, 241, 383, 94, 462, 53, 457, 282],
-        fillColor: "yellow"
-      },
-      {
-        name: "4",
-        shape: "poly",
-        coords: [245, 285, 290, 285, 274, 239, 249, 238],
-        preFillColor: "red"
-      },
-      { name: "5", shape: "circle", coords: [170, 100, 25] }
-    ]
-  });
-
-  const [MAPT, setMAPT] = useState({
+  const [userLayout, setUserLayout] = useState({
     name: "test_map",
     areas: [
       {
@@ -54,59 +22,9 @@ const app = (props) => {
 
   const [Dots, setDots] = useState([]);
 
-  let DotsX = {
+  let AdminLayout = {
     name: "dots-map",
     areas: Dots
-  };
-
-  let MAP = {
-    name: "my-map",
-    areas: [
-      {
-        name: "1",
-        shape: "poly",
-        coords: [25, 33, 27, 300, 128, 240, 128, 94],
-        preFillColor: "green",
-        fillColor: "#0000ff"
-      },
-      {
-        name: "2",
-        shape: "poly",
-        coords: [219, 118, 220, 210, 283, 210, 284, 119],
-        preFillColor: "pink",
-        lineWidth: 10,
-        strokeColor: "#0000ff"
-      },
-      {
-        name: "3",
-        shape: "poly",
-        coords: [381, 241, 383, 94, 462, 53, 457, 282],
-        preFillColor: "yellow", // this is mandatory for stroke color to work
-        lineWidth: 10,
-        strokeColor: "#6afd09"
-      },
-      {
-        name: "4",
-        shape: "poly",
-        coords: [245, 285, 290, 285, 274, 239, 249, 238],
-        preFillColor: "red"
-      },
-      {
-        name: "5",
-        shape: "circle",
-        coords: [170, 100, 25],
-        preFillColor: "rgb(255,255,255,0.3)",
-        lineWidth: 2
-      },
-      {
-        name: "6",
-        shape: "rect",
-        coords: [270, 100, 200, 50],
-        lineWidth: 2,
-        preFillColor: "rgba(255, 255, 255, 0.3)",
-        strokeColor: "#6afd09"
-      }
-    ]
   };
 
   var URL = "https://c1.staticflickr.com/5/4052/4503898393_303cfbc9fd_b.jpg";
@@ -124,7 +42,7 @@ const app = (props) => {
   };
 
   const clickHandler = (x, y) => {
-    setMAP2({
+    setUserLayout({
       name: "my-map",
       areas: [
         {
@@ -133,28 +51,10 @@ const app = (props) => {
           coords: [25, 33, 27, 300, 128, 240, x, y],
           preFillColor: "green",
           fillColor: "blue"
-        },
-        {
-          name: "2",
-          shape: "poly",
-          coords: [219, 118, 220, 210, 283, 210, 284, 119],
-          preFillColor: "pink"
-        },
-        {
-          name: "3",
-          shape: "poly",
-          coords: [381, 241, 383, 94, 462, 53, 457, 282],
-          fillColor: "yellow"
-        },
-        {
-          name: "4",
-          shape: "poly",
-          coords: [245, 285, 290, 285, 274, 239, 249, 238],
-          preFillColor: "red"
-        },
-        { name: "5", shape: "circle", coords: [170, 100, 25] }
+        }
       ]
     });
+
     // MAP2.areas.push({name: "10",
     // shape: "poly",
     // coords: arr,
@@ -163,7 +63,8 @@ const app = (props) => {
 
   const clickedOutside = (evt) => {
     const coords = { x: evt.nativeEvent.layerX, y: evt.nativeEvent.layerY };
-    setMsg(`You clicked on the image at coords ${JSON.stringify(coords)} !`);
+    // setMsg(`You clicked on the image at coords ${JSON.stringify(coords)} !`);
+
     setDots((oldArray) => [
       ...oldArray,
       {
@@ -221,6 +122,9 @@ const app = (props) => {
     return { top: `${area.center[1]}px`, left: `${area.center[0]}px` };
   };
 
+  let coordsT = [];
+  const [coordsU, setCoordsU] = useState([]);
+
   const makeDot = (evt) => {
     const coords = { x: evt.nativeEvent.layerX, y: evt.nativeEvent.layerY };
     setDots((oldArray) => [
@@ -233,30 +137,25 @@ const app = (props) => {
         lineWidth: 11
       }
     ]);
-    handleChangeObjectAddArray(666);
-  };
+    coordsT.push(coords.x);
+    coordsT.push(coords.y);
 
-  const [Dots2, setDots2] = useState([40]);
+    setCoordsU((oldArray) => [...oldArray, coords.x]);
+    setCoordsU((oldArray) => [...oldArray, coords.y]);
 
-  let DotsT = {
-    name: "dots-map2",
-    areas: []
+    console.log(coordsT);
+    console.log(coordsU);
   };
 
   const handleChangeObjectAddArray = (coord, index) => {
-    DotsT.areas.push(4);
-    setDots2(DotsT);
     //console.log(MAPT.areas[0].coords[0])
-    {
-      console.log(Dots2);
-    }
   };
 
   return (
     <div className="grid">
       <div className="presenter">
         <div style={{ position: "relative" }}>
-          <ImageMapper
+          {/* <ImageMapper
             src={URL}
             map={MAP2}
             width={500}
@@ -269,34 +168,40 @@ const app = (props) => {
             onImageMouseMove={(evt) => moveOnImage(evt)}
             lineWidth={4}
             strokeColor={"white"}
-          />
+          /> */}
 
-          <ImageMapper
-            src={URL}
-            map={DotsX}
-            width={500}
-            onImageClick={(evt) => makeDot(evt)}
-            onImageMouseMove={(evt) => moveOnImage(evt)}
-          />
-
-          <ImageMapper src={URL} map={MAPT} width={500} />
-
-          {hoveredArea && (
+          {/* {hoveredArea && (
             <span
               className="tooltip"
               style={{ ...getTipPosition(hoveredArea) }}
             >
               {hoveredArea && hoveredArea.name}
             </span>
-          )}
-        </div>
+          )} */}
 
-        <pre className="message">{msg ? msg : null}</pre>
-        <pre>{moveMsg ? moveMsg : null}</pre>
+          <h2>Edit Interface</h2>
+          <ImageMapper
+            src={URL}
+            map={AdminLayout}
+            width={500}
+            onImageClick={(evt) => makeDot(evt)}
+            onImageMouseMove={(evt) => moveOnImage(evt)}
+          />
+
+          <h2>User Interface</h2>
+          <ImageMapper
+            src={URL}
+            map={userLayout}
+            width={500}
+            onImageClick={(evt) => clickedOutside(evt)}
+            onImageMouseMove={(evt) => moveOnImage(evt)}
+          />
+        </div>
+        {/* <pre className="message">{msg ? msg : null}</pre> */}
+        {/* <pre>{moveMsg ? moveMsg : null}</pre> */}
       </div>
     </div>
   );
-  //return React.createElement('div', {className:"App"}, React.createElement('h1',null,'Hi 2 !!!'))
 };
 
 export default app;
