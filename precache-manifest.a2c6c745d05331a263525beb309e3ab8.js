78511c980827d881b81a5e6bcc3a31ddabb8d1f5
MJ <michaljalowik@gmail.com>
2021-03-20 17:34:07 +0100

    Updates

---
+++ precache-manifest.a2c6c745d05331a263525beb309e3ab8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "11b3e1efacd18a3cb1b6",
+    "url": "/React_Course/static/css/main.1422b82e.chunk.css"
+  },
+  {
+    "revision": "11b3e1efacd18a3cb1b6",
+    "url": "/React_Course/static/js/main.a524a3f9.chunk.js"
+  },
+  {
+    "revision": "1402094d24e37d67dd8e",
+    "url": "/React_Course/static/js/runtime~main.8f041fa1.js"
+  },
+  {
+    "revision": "da4034de9add36a36dd2",
+    "url": "/React_Course/static/js/2.b317a35f.chunk.js"
+  },
+  {
+    "revision": "bfe90b92b65432c31633f3616dea25a9",
+    "url": "/React_Course/index.html"
+  }
+];
