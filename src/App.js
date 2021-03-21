10bef9f5bcc4df03aebe80e5efee6609ecbaa11e
MJ <michaljalowik@gmail.com>
2021-03-21 10:27:23 +0100

    test deploy

--- src/App.js
+++ src/App.js
@@ -215,7 +215,7 @@ const app = (props) => {
         <div style={{ position: "relative" }}>
           {adminMode ? (
             <div className="presenter">
-              <h2>Admin Layout</h2>
+              <h2>Admin layout</h2>
               <ImageMapper
                 src={URL}
                 map={adminLayout}
