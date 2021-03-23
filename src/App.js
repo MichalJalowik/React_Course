a7619981442b3b434fbfd9cd864b85aa5ee4b7bd
Michał Jałowik <57842170+MichalJalowik@users.noreply.github.com>
2021-03-23 08:15:25 +0100

    v2

--- src/App.js
+++ src/App.js
@@ -12,17 +12,20 @@ import {
 
 const app = () => {
   return (
-    <MapContainer center={[51.505, -0.091]} zoom={13}>
-      <TileLayer
-        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.091]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <div>
+      <h1>React Leaflet</h1>
+      <MapContainer center={[51.505, -0.091]} zoom={13}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.091]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
   );
 };
 
